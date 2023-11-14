80dd1107ad2922e56753f9267a24d6d698319c1f
Lucas <llgoddin@gmail.com>
2023-11-14 15:23:17 -0500

    added changes in container size

--- Graphics.py
+++ Graphics.py
@@ -9,46 +9,48 @@ from Dashboard import Dashboard
 
 def update_container_settings(p_manager, dash):
     for element in dash.get_elements():
-        match element.func_str:
-            case 'Gravity':
-                p_manager.set_gravity(element.active)
+        if element.get_clicked():
+            match element.func_str:
+                case 'Gravity':
+                    p_manager.set_gravity(element.active)
 
-            case 'Repulse':
-                p_manager.set_repulse(element.active)
+                case 'Repulse':
+                    p_manager.set_repulse(element.active)
 
-            case '+ Particle':
-                if element.get_clicked():
+                case '+ Particle':
                     p_manager.spawn_particle()
 
-            case '- Particle':
-                if element.get_clicked():
+                case '- Particle':
                     p_manager.delete_particle()
 
-            #TO DO - IMPLEMENT
-            case '+ Container Width':
-                #p_manager.increase_container_width()
-                continue
-            case '- Container Width':
-                #p_manager.decrease_container_width()
-                continue
-            case '+ Container Height':
-                #p_manager.increase_container_height()
-                continue
-            case '- Container Height':
-                #p_manager.decrease_container_height()
-                continue
-            case '+ Repulse Force':
-                #p_manager.increase_repulse()
-                continue
-            case '- Repulse Force':
-                #p_manager.decrease_repulse()
-                continue
+                #TO DO - IMPLEMENT
+                case '+ Container Width':
+                    p_manager.increase_container_width()
 
+                case '- Container Width':
+                    p_manager.decrease_container_width()
+
+                case '+ Container Height':
+                    p_manager.increase_container_height()
+
+                case '- Container Height':
+                    p_manager.decrease_container_height()
+
+                case '+ Repulse Force':
+                    #p_manager.increase_repulse()
+                    continue
+                case '- Repulse Force':
+                    #p_manager.decrease_repulse()
+                    continue
 
 grav_btn = Button(10, 10, 100, 40, (0, 0, 200), 'Gravity', True, 'Gravity')
 repulse_btn = Button(10, 60, 100, 40, (80, 0, 200), 'Repulse', True, 'Repulse')
 add_particle_btn = Button(10, 110, 100, 40, (120, 0, 200), '+ Particles', False, '+ Particle')
 subtract_particle_btn = Button(10, 170, 100, 40, (80, 30, 200), '- Particles', False, '- Particle')
+increase_container_width_btn = Button(10, 230, 100, 40, (80, 30, 200), '+ Width', False, '+ Container Width')
+decrease_container_width_btn = Button(10, 290, 100, 40, (80, 30, 200), '- Width', False, '- Container Width')
+increase_container_height_btn = Button(10, 350, 100, 40, (80, 30, 200), '+ Height', False, '+ Container Height')
+decrease_container_height_btn = Button(10, 410, 100, 40, (80, 30, 200), '- Height', False, '- Container Height')
 
 dash = Dashboard(600, 0, 200, 800)
 
@@ -56,6 +58,10 @@ dash.add_element(grav_btn)
 dash.add_element(repulse_btn)
 dash.add_element(add_particle_btn)
 dash.add_element(subtract_particle_btn)
+dash.add_element(increase_container_width_btn)
+dash.add_element(decrease_container_width_btn)
+dash.add_element(increase_container_height_btn)
+dash.add_element(decrease_container_height_btn)
 
 pygame.init()
 CLOCK = pygame.time.Clock()
@@ -67,6 +73,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 FPS = 120
+DASH_FPS = FPS / 10
 
 SLOW_TIME_BY = 1
 
@@ -83,7 +90,9 @@ CONTAINER = (200, 200)
 # Calculated Variables
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Liquid Simulator")
-liquid = Particle_Manager(num_particles=NUM_PARTICLES, radius=5, x_bounds=CONTAINER[0]/2, y_bounds=CONTAINER[1]/2, gravity=GRAVITY, repulse=REPULSE, vertical_spawn=VERTICAL_SPAWN)
+liquid = Particle_Manager(x=(SCREEN_WIDTH/2 - 100), y=SCREEN_HEIGHT/2, num_particles=NUM_PARTICLES, radius=5, x_bounds=CONTAINER[0]/2, y_bounds=CONTAINER[1]/2, gravity=GRAVITY, repulse=REPULSE, vertical_spawn=VERTICAL_SPAWN)
+
+dash_tick = 0
 
 now = 0
 prev_time = None
@@ -94,6 +103,8 @@ run = True
 while run:
 
     CLOCK.tick(FPS)
+
+    dash_tick += 1
 
     now = time.time()
 
@@ -114,16 +125,20 @@ while run:
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
 
-            dash.check_click(pos)
+            if pos[0] > dash.x:
+                dash.check_click(pos)
+            else:
+                print("else")
+                liquid.apply_mouse_force(pos)
 
         if event.type == pygame.MOUSEBUTTONUP:
             dash.reset_clicked_elements()
 
     update_container_settings(liquid, dash)
 
-    dash.render(screen=screen)
-
     liquid.update(screen=screen, dt=dt)
+
+    dash.render(screen=screen)
 
     pygame.display.update()
 
--- Particle_Manager.py
+++ Particle_Manager.py
@@ -4,13 +4,15 @@ import math, pygame, random
 
 class Particle_Manager():
 
-    def __init__(self, num_particles, radius, x_bounds, y_bounds, gravity, repulse, vertical_spawn):
+    def __init__(self, x, y, num_particles, radius, x_bounds, y_bounds, gravity, repulse, vertical_spawn):
         self.GRAVITY = gravity
         self.REPULSE = repulse
 
         self.particles = []
         self.num_particles = num_particles
         self.particle_radius = radius
+
+        self.x, self.y = x, y
 
         self.X_BOUND = x_bounds
         self.Y_BOUND = y_bounds
@@ -24,13 +26,12 @@ class Particle_Manager():
 
         i = 0
 
+        self.update_bounds(x_bounds, y_bounds)
+
         for x in range(square):
             for y in range(square):
 
                 if i < len(self.particles):
-                    self.particles[i].bind_x(x_bounds, -x_bounds)
-                    self.particles[i].bind_y(y_bounds, -y_bounds)
-
                     if vertical_spawn:
                         self.particles[i].x = (buffer + radius) * x
                         self.particles[i].y = (buffer + radius) * y
@@ -42,40 +43,84 @@ class Particle_Manager():
 
                 i += 1
     
-    def render_bounds(self, screen):
-        X_OFFSET = screen.get_width()/2
-        Y_OFFSET = -screen.get_height()/2
+    # Update methods
+    def spawn_particle(self):
+
+        new_particle = Particle(random.randint(-5,5), random.randint(-5,5), self.particle_radius)
+
+        new_particle.bind_x(self.X_BOUND, -self.X_BOUND)
+        new_particle.bind_y(self.Y_BOUND, -self.Y_BOUND)
         
-        RENDER_X = -self.X_BOUND + X_OFFSET
-        RENDER_Y = screen.get_height() - self.Y_BOUND + Y_OFFSET
+        self.particles.append(new_particle)
+        self.num_particles += 1
+
+    def delete_particle(self):
+        if len(self.particles) > 1:
+            self.num_particles -= 1
+            self.particles.pop()
+
+    def increase_container_width(self):
+        self.update_bounds(self.X_BOUND + 1, self.Y_BOUND)
+
+    def decrease_container_width(self):
+        self.update_bounds(self.X_BOUND - 1, self.Y_BOUND)
+
+    def increase_container_height(self):
+        self.update_bounds(self.X_BOUND, self.Y_BOUND + 1)
+
+    def decrease_container_height(self):
+        self.update_bounds(self.X_BOUND, self.Y_BOUND - 1)
+
+    def update_bounds(self, x_bound, y_bound):
+        self.X_BOUND = x_bound
+        self.Y_BOUND = y_bound
+
+        for particle in self.particles:
+            particle.bind_x(x_bound, -x_bound)
+            particle.bind_y(y_bound, -y_bound)
+    
+    def update_coordinate_grid(self, screen):
+        # X_OFFSET = screen.get_width()/2
+        # Y_OFFSET = -screen.get_height()/2
+        
+        # RENDER_X = particle.x + X_OFFSET
+        # RENDER_Y = screen.get_height() - particle.y + Y_OFFSET
+        return 0
+
+    def apply_mouse_force(self, pos):
+        # X_OFFSET = screen.get_width()/2
+        # Y_OFFSET = -screen.get_height()/2
+        
+        # RENDER_X = particle.x + X_OFFSET
+        # RENDER_Y = screen.get_height() - particle.y + Y_OFFSET
+
+        # for particle in self.particles:
+        #     particle.force_towards_mouse(pos)
+        return 0
+
+    # Render Methods
+    def render_bounds(self, screen):
+        RENDER_X = -self.X_BOUND + self.x
+        RENDER_Y = -self.Y_BOUND + self.y
 
         pygame.draw.rect(screen, (40, 170, 40), pygame.Rect(RENDER_X, RENDER_Y, self.X_BOUND * 2, self.Y_BOUND * 2), 3)
 
     def render_particle(self, screen, particle):
-        X_OFFSET = screen.get_width()/2
-        Y_OFFSET = -screen.get_height()/2
-        
-        RENDER_X = particle.x + X_OFFSET
-        RENDER_Y = screen.get_height() - particle.y + Y_OFFSET
+        RENDER_X = particle.x + self.x
+        RENDER_Y = -particle.y + self.y
 
         pygame.draw.circle(screen, particle.color, (RENDER_X, RENDER_Y), particle.radius)
 
     def render_particle_influence(self, screen, particle):
-        X_OFFSET = screen.get_width()/2
-        Y_OFFSET = -screen.get_height()/2
-        
-        RENDER_X = particle.x + X_OFFSET
-        RENDER_Y = screen.get_height() - particle.y + Y_OFFSET
+        RENDER_X = particle.x + self.x
+        RENDER_Y = -particle.y + self.y
 
         influence_color = (240, 220, 220)
         pygame.draw.circle(screen, influence_color, (RENDER_X, RENDER_Y), particle.influence_radius)
         
     def render_particle_velocity(self, screen, particle):
-        X_OFFSET = screen.get_width()/2
-        Y_OFFSET = -screen.get_height()/2
-        
-        RENDER_X = particle.x + X_OFFSET
-        RENDER_Y = screen.get_height() - particle.y + Y_OFFSET
+        RENDER_X = particle.x + self.x
+        RENDER_Y = -particle.y + self.y
 
         if abs(particle.velocity.x) > particle.influence_radius:
             line_x = particle.influence_radius * (abs(particle.velocity.x)/particle.velocity.x)
@@ -89,9 +134,6 @@ class Particle_Manager():
         pygame.draw.line(screen, (255, 0, 0), (RENDER_X, RENDER_Y), (RENDER_X + line_x, RENDER_Y - line_y), 2)
 
     def update(self, screen, dt):
-        X_OFFSET = screen.get_width()/2
-        Y_OFFSET = -screen.get_height()/2
-
         # Calculate
         for i in range(len(self.particles)):
             if self.REPULSE:
@@ -123,32 +165,21 @@ class Particle_Manager():
         # Render Layer 3
         for particle in self.particles:
             self.render_particle(screen, particle)
-        
+
+    # Getters    
     def get_gravity(self):
         return self.GRAVITY
-    
-    def set_gravity(self, value=False):
-        if value == True or value == False:
-            self.GRAVITY = value
 
     def get_repulse(self):
         return self.REPULSE
     
+
+    # Setters
+    def set_gravity(self, value=False):
+        if value == True or value == False:
+            self.GRAVITY = value
+
     def set_repulse(self, value=False):
         if value == True or value == False:
             self.REPULSE = value
-
-    def spawn_particle(self):
-
-        new_particle = Particle(random.randint(-5,5), random.randint(-5,5), self.particle_radius)
-
-        new_particle.bind_x(self.X_BOUND, -self.X_BOUND)
-        new_particle.bind_y(self.Y_BOUND, -self.Y_BOUND)
-        
-        self.particles.append(new_particle)
-        self.num_particles += 1
-
-    def delete_particle(self):
-        if len(self.particles) > 1:
-            self.num_particles -= 1
-            self.particles.pop()
+    
